12894066f28e51b8e3a2d2160c57781a8ad1fefb
karimGl <karim.goubaa@gmail.com>
2025-05-17 01:13:12 +0200

    feat(history): transactions history feature minimal implementation

--- src/main/java/com/moneta/account/Account.java
+++ src/main/java/com/moneta/account/Account.java
@@ -4,15 +4,19 @@ import com.moneta.operation.*;
 import com.moneta.transaction.Transaction;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
 
     private BigDecimal balance = BigDecimal.ZERO;
 
+    private final List<Transaction> transactions = new ArrayList<>();
+
     public void deposit(BigDecimal amount) {
         Transaction transaction = new DepositOperation(amount, balance).execute();
         balance = transaction.balanceAfter();
+        transactions.add(transaction);
     }
 
     public BigDecimal balance() {
@@ -23,9 +27,10 @@ public class Account {
         Operation operation = new WithdrawOperation(amount, balance);
         Transaction transaction = operation.execute();
         balance = transaction.balanceAfter();
+        transactions.add(transaction);
     }
 
     public List<Transaction> history() {
-        return null;
+        return transactions;
     }
 }
